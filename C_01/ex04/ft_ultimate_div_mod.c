2204bc747ebd27151b50b86cd6c410d44005760d
mkawk <kacamimi@naver.com>
2022-02-07 17:46:40 +0900

    commit

--- C_01/ex04/ft_ultimate_div_mod.c
+++ C_01/ex04/ft_ultimate_div_mod.c
@@ -6,12 +6,15 @@
 /*   By: mkwak <mkwak@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 11:20:33 by mkwak             #+#    #+#             */
-/*   Updated: 2022/02/07 14:15:47 by mkwak            ###   ########.fr       */
+/*   Updated: 2022/02/07 17:28:42 by mkwak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*a = *a / *b;
-	*b = *a % *b;
+	int a_div_b = *a / *b;
+	int a_mod_b = *a % *b;
+
+	*a = a_div_b;
+	*b = a_mod_b;
 }
