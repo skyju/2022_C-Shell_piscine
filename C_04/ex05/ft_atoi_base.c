4742dfe0ea60b1f21968c2be3df720f6f245c5ae
skyju <kacamimi3544@gmail.com>
2022-02-13 22:21:53 +0900

    commit

--- C_04/ex05/ft_atoi_base.c
+++ C_04/ex05/ft_atoi_base.c
@@ -32,7 +32,9 @@ int	ft_base_checking(char *base)
 	check = 0;
 	while (*(base + i))
 	{
-		if (*(base + i) == '-' || *(base + i) == '+' || *(base + i) == ' ')
+		if (*(base + i) == '-' || *(base + i) == '+' || *(base + i) == ' '
+			|| *(base + i) == '\f' || *(base + i) == '\n' || *(base + i) == '\r'
+			|| *(base + i) == '\t' || *(base + i) == '\v')
 			check = 1;
 		if (ft_my_strstr((base + i + 1), *(base + i)))
 			check = 1;
@@ -48,7 +50,7 @@ int	ft_base_cal(char c, char *base)
 	i = 0;
 	while (*(base + i))
 	{
-		if (c == *(base + i) || c == *(base + i))
+		if (c == *(base + i))
 			return (i);
 		++i;
 	}
@@ -100,4 +102,38 @@ int	ft_atoi_base(char *str, char *base)
 		++i;
 	result = ft_calculator(str, i, obase, base);
 	return (result);
+}
+
+int main(void)
+{
+	char *str1 = "   ---+--+AABCDESSEZ";
+	char *str2 = "  \n \ta-a";
+	char *str3 = " \v  ++2147483647";
+	char *str4 = " \f  -----+-+!!@@#..<";
+	char *str5 = "\r  \n -+-++-+2147483648";
+	printf("10\n");
+	printf("%d\n", ft_atoi_base("---+-1010", "01")); //2base
+	printf("\n");
+	printf("-67174\n");
+	printf("%d\n", ft_atoi_base(str1, "ABCDES")); // 6base
+	printf("\n");
+	printf("-1\n");
+	printf("%d\n", ft_atoi_base(str1, "ABZ")); 	
+	printf("\n");
+	printf("0\n");
+	printf("%d\n", ft_atoi_base(str3, "A-+")); // +- include
+	printf("\n");
+	printf("0\n");
+	printf("%d\n", ft_atoi_base(str1, "")); // 0base
+	printf("\n");
+	printf("-2147483648\n");
+	printf("%d\n", ft_atoi_base(str5, "0123456789")); // 10base
+	printf("\n");
+	printf("2\n");
+	printf("%d\n", ft_atoi_base(str2, "cba")); // 3base
+	printf("\n");
+	printf("1126397\n");
+	printf("%d\n", ft_atoi_base(str4, "!@#$%^&*()~?><'.")); // 16base
+	printf("\n");
+	return 0;
 }
