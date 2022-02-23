f16f6d6d969bf12cd77e039c1f663e3b20391d70
skyju <77267141+skyju@users.noreply.github.com>
2022-02-23 12:37:27 +0900

    Update ft_is_sort.c

--- C_11/ex04/ft_is_sort.c
+++ C_11/ex04/ft_is_sort.c
@@ -12,26 +12,23 @@
 
 #include <stdio.h>
 
-int	ft_is_sort(int *tab, int length, int(*f)(int, int))
+int	is_ascending(int length, int *tab, int(*f)(int, int))
 {
 	int	i;
-	int	j;
+	int j;
 	int	result;
-	int	temp;
-	int	temp2;
 	int	flag;
 
 	i = 0;
+	result = 0;
 	flag = 0;
 	while (i < length - 1)
 	{
 		j = i + 1;
 		while (j < length - 2)
 		{
-			temp = *(tab + i);
-			temp2 = *(tab + j);
-			result = (*f)(temp, temp2);
-			if (flag > 0 && result < 0)
+			result = (*f)(*(tab + i), *(tab + j));
+			if (flag >= 0 && result < 0)
 				return (0);
 			flag = result;
 			++j;
@@ -39,6 +36,39 @@ int	ft_is_sort(int *tab, int length, int(*f)(int, int))
 		++i;
 	}
 	return (1);
+}
+
+int	is_descending(int length, int *tab, int(*f)(int, int))
+{
+	int	i;
+	int j;
+	int	result;
+	int	flag;
+
+	i = 0;
+	result = 0;
+	flag = 0;
+	while (i < length - 1)
+	{
+		j = i + 1;
+		while (j < length - 2)
+		{
+			result = (*f)(*(tab + i), *(tab + j));
+			if (flag <= 0 && result > 0)
+				return (0);
+			flag = result;
+			++j;
+		}
+		++i;
+	}
+	return (1);
+}
+
+int	ft_is_sort(int *tab, int length, int(*f)(int, int))
+{
+	if (is_ascending(length, tab, f) || is_descending(length, tab, f))
+		return (1);
+	return (0);
 }
 
 int return_min(int a, int b)
@@ -49,15 +79,15 @@ int return_min(int a, int b)
 int	main(void)
 {
 	printf("\n--ex04--\n");
-	int arr1[10] = {1, 2, 9, 8, 3, 4, 6, 5, 10, 11};
+	int arr1[10] = {1, 2, 9, 9, 9, 5, 4, 4, 10, 11};
 	for(int i=0;i<10;i++)
 		printf("%d ", arr1[i]);
 	printf(": %d\n", ft_is_sort(arr1, 10, &return_min));
-	int arr2[10] = {-10, 2, 3, 4, 5, 6, 7, 8, 10, 112};
+	int arr2[10] = {-10, 2, 3, 3, 5, 6, 7, 8, 10, 112};
 	for(int i=0;i<10;i++)
 		printf("%d ", arr2[i]);
 	printf(": %d\n", ft_is_sort(arr2, 10, &return_min));
-	int arr3[10] = {11, 9, 6, 3, 1, 0, -1, -13, -14, -15};
+	int arr3[11] = {11, 9, 6, 3, 1, 1, -1, -13, -14, -14, -15};
 	for(int i=0;i<10;i++)
 		printf("%d ", arr3[i]);
 	printf(": %d\n", ft_is_sort(arr3, 10, &return_min));
